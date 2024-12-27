2049cd5354b46509903d36974fecfa3a62b29404
suryaalmajid1810 <suryaalmajid@gmail.com>
2024-12-28 02:49:56 +0700

    User features

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,16 @@
 
 namespace App\Providers;
 
+use App\Models\Ingredient;
+use App\Models\Post;
+use App\Models\Recipe;
+use App\Models\Restaurant;
+use App\Models\User;
+use App\Observers\IngredientObserver;
+use App\Observers\PostObserver;
+use App\Observers\RecipeObserver;
+use App\Observers\RestaurantObserver;
+use App\Observers\UserObserver;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +29,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Recipe::observe(RecipeObserver::class);
+        Restaurant::observe(RestaurantObserver::class);
+        Ingredient::observe(IngredientObserver::class);
+        Post::observe(PostObserver::class);
+        User::observe(UserObserver::class);
     }
 }
